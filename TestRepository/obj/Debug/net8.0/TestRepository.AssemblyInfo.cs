ff5bf819199bcf62f052b687f1ac12958074c693
Bugaenko Vladislav <bugaenkovlad201@gmail.com>
2024-03-25 21:32:49 +0500

    deleted unusedfiles

--- TestRepository/obj/Debug/net8.0/TestRepository.AssemblyInfo.cs
+++ TestRepository/obj/Debug/net8.0/TestRepository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8587cc2186d1244400568c3aa7a81f2af62e827")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WorkerService1/Program.cs
+++
@@ -1,7 +0,0 @@
-using WorkerService1;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
